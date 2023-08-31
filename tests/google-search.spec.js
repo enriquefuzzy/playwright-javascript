3dd182b34e394c433795e88ccab6c3540716bf90
Enrique Aguirre <enrique@yourfuzzy.com>
2023-08-31 15:30:44 -0700

    Adjust Dog selector using codegen selector

--- tests/google-search.spec.js
+++ tests/google-search.spec.js
@@ -6,10 +6,10 @@ test('Verify user can search for Dogs', async ({page})=>{
 
     await page.getByTitle('Search').fill('Dogs');
     await page.getByTitle('Search').press('Enter');
-    // await expect(page).toHaveURL(new RegExp('.*search?q=Dogs'));
     // await expect(page.getByRole('searchbox', {name: 'q'})).toHaveText('Dogs');
     // await expect(page.getByRole('textbox', {name:'q'})).toHaveText('Dogs');
-    await expect(page.locator('textarea[name=q]')).toHaveText('Dogs');
+    // await expect(page.locator('textarea[name=q]')).toHaveText('Dogs');
+    await expect(page.getByRole('heading', { name: 'Dog', exact: true })).toHaveText('Dog');
     // await expect(page.getByRole('heading', {level: 2})).toHaveText('Dog');
     await expect(page.locator('[data-attrid=title]')).toHaveText('Dog');
 });
