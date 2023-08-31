44d7c867762ae36640668a56dd665c78a45eedd0
Enrique Aguirre <enrique@yourfuzzy.com>
2023-08-31 15:22:44 -0700

    Create Dogs search test

---
+++ tests/google-search.spec.js
@@ -0,0 +1,15 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test('Verify user can search for Dogs', async ({page})=>{
+    await page.goto('https://www.google.com');
+
+    await page.getByTitle('Search').fill('Dogs');
+    await page.getByTitle('Search').press('Enter');
+    // await expect(page).toHaveURL(new RegExp('.*search?q=Dogs'));
+    // await expect(page.getByRole('searchbox', {name: 'q'})).toHaveText('Dogs');
+    // await expect(page.getByRole('textbox', {name:'q'})).toHaveText('Dogs');
+    await expect(page.locator('textarea[name=q]')).toHaveText('Dogs');
+    // await expect(page.getByRole('heading', {level: 2})).toHaveText('Dog');
+    await expect(page.locator('[data-attrid=title]')).toHaveText('Dog');
+});
