7a5d45beafed3d6e322bb628328e8b4e86c078fd
Enrique Aguirre <enrique@yourfuzzy.com>
2023-08-31 19:34:27 -0700

    Automate Dog and Cat Google search

--- tests/google-search.spec.js
+++ tests/google-search.spec.js
@@ -3,13 +3,18 @@ const { test, expect } = require('@playwright/test');
 
 test('Verify user can search for Dogs', async ({page})=>{
     await page.goto('https://www.google.com');
-
     await page.getByTitle('Search').fill('Dogs');
     await page.getByTitle('Search').press('Enter');
-    // await expect(page.getByRole('searchbox', {name: 'q'})).toHaveText('Dogs');
-    // await expect(page.getByRole('textbox', {name:'q'})).toHaveText('Dogs');
-    // await expect(page.locator('textarea[name=q]')).toHaveText('Dogs');
+    await expect(page.getByRole('combobox', { name: 'Search' })).toHaveText('Dogs');
+    // await expect(page.locator('textarea[name=q]')).toHaveText('Dogs'); - Alternate CSS assertion
     await expect(page.getByRole('heading', { name: 'Dog', exact: true })).toHaveText('Dog');
-    // await expect(page.getByRole('heading', {level: 2})).toHaveText('Dog');
-    await expect(page.locator('[data-attrid=title]')).toHaveText('Dog');
+    // await expect(page.locator('[data-attrid=title]')).toHaveText('Dog'); - Alternate CSS assertion
+});
+
+test('Verify user can search for Cats', async ({page})=>{
+    await page.goto('https://www.google.com');
+    await page.getByTitle('Search').fill('Cats');
+    await page.getByTitle('Search').press('Enter');
+    await expect(page.getByRole('combobox', { name: 'Search' })).toHaveText('Cats');
+    await expect(page.locator('[data-attrid=title]')).toHaveText('Cat');
 });
